e87c89d0fdccb9bfbe94af158f92e88720e18351
Paulo Lobito <235763087+PauloLobito@users.noreply.github.com>
2026-03-07 21:42:42 +0000

    feat(project):cli.py updated

--- project/src/cli.py
+++ project/src/cli.py
@@ -1,27 +1,18 @@
+
 import typer
 from src.logic import analyze_text
 
 app = typer.Typer()
 
-
 @app.command()
 def analyze(file: str):
-    """
-    Analyze a text file and show statistics.
-    """
-
+    """Analyze a text file and show statistics."""
     try:
         result = analyze_text(file)
-
         print(f"Lines: {result['lines']}")
         print(f"Words: {result['words']}")
         print(f"Characters: {result['characters']}")
-
     except FileNotFoundError:
         print("Error: File not found.")
     except Exception as e:
         print(f"Unexpected error: {e}")
-
-
-if __name__ == "__main__":
-    app()
