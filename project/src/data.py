bebe69261ff54cd2b329bb31bb519859a7e86dce
Paulo Lobito <235763087+PauloLobito@users.noreply.github.com>
2026-03-07 21:18:31 +0000

    feat(project):data.py created

---
+++ project/src/data.py
@@ -0,0 +1,15 @@
+from data import read_file
+
+
+def analyze_text(filepath: str):
+    text = read_file(filepath)
+
+    lines = text.count("\n") + 1
+    words = len(text.split())
+    characters = len(text)
+
+    return {
+        "lines": lines,
+        "words": words,
+        "characters": characters
+    }
